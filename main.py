ce8f5b94567b13a3e61d86bc7e1ccfb1251b8863
filiperak <filip.erak@googlemail.com>
2024-10-12 19:16:52 +0200

    path

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return{"message":"Filip 1312"}
+def root():
+    return{"message":"Filip 1312 "}          #sam ga prevede u JSON
+
