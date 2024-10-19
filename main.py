8dee8decc9a57ff6bdb9dda95efc6a4cf94a948a
filiperak <filip.erak@googlemail.com>
2024-10-19 15:00:20 +0200

    post update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -10,20 +11,37 @@ class Post(BaseModel):
     published:bool = True
     rating: Optional[float] = None
 
+my_posts = [
+    {"title":"title of post 1","content":"content of post1","id":1},
+    {"title":"title of post 2","content":"content of post2","id":2}
+    ]
+
 @app.get("/")
 def root():
     return {"data":"data"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"postdata"}
+    return {"data":my_posts}
 
 # @app.post("/createpost")
 # def create_posts(payload:dict = Body(...)):
 #     print(payload)
 #     return {"new_post":f"title: {payload['title']} , content: {payload['content']}"}
 
-@app.post("/createpost")
+# @app.post("/createpost")
+# def create_posts(new_post:Post):
+#     print(new_post)
+#     print(new_post.dict())
+#     return {"new_post":new_post}
+
+
+@app.post("/posts")
 def create_posts(new_post:Post):
-    print(new_post.published)
+    #print(new_post)
+    #print(new_post.model_dump())
+    post_dict = new_post.model_dump()
+    post_dict['id'] = randrange(0,1000)
+    my_posts.append(post_dict)
+
     return {"new_post":new_post}
