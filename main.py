6acc051ba4bf10ebb50754436d41628e5ae5c77c
filiperak <filip.erak@googlemail.com>
2024-10-19 14:23:14 +0200

    basemodel

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published:bool = True
+    rating: Optional[float] = None
 
 @app.get("/")
 def root():
@@ -10,8 +18,12 @@ def root():
 def get_posts():
     return {"data":"postdata"}
 
+# @app.post("/createpost")
+# def create_posts(payload:dict = Body(...)):
+#     print(payload)
+#     return {"new_post":f"title: {payload['title']} , content: {payload['content']}"}
+
 @app.post("/createpost")
-def create_posts(payload:dict = Body(...)):
-    print(payload)
-    return {"new_post":f"title: {payload['title']} , content: {payload['content']}"}
-    
+def create_posts(new_post:Post):
+    print(new_post.published)
+    return {"new_post":new_post}
